ba666ef16d51694bac6823c86eeb072a89716e37
Anwai Archit <anwai.archit@gmail.com>
2024-04-13 00:03:19 +0200

    Add module setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tukra',
+    version='0.0.1',
+    description='Functionality for evaluating deep learning-based segmentation methods.',
+    author='Anwai Archit',
+    author_email='anwai.archit@uni-goettingen.de',
+    url='https://user.informatik.uni-goettingen.de/~pape41/',
+    packages=['tukra'],
+)
