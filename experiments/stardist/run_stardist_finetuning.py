7cddb2e7c92b64dc5ff3f83c4751a4d056f48f11
anwai98 <anwai.archit@gmail.com>
2024-07-27 22:55:00 +0200

    Add stardist training scripts

--- experiments/stardist/run_stardist_finetuning.py
+++ experiments/stardist/run_stardist_finetuning.py
@@ -1,33 +1,25 @@
-from stardist import gputools_available
-from stardist.models import Config2D, StarDist2D
+import os
+from glob import glob
+from natsort import natsorted
+
+from tukra.training import run_stardist_training
 
 
-def run_stardist_finetuning(
-    train_image_paths,
-    train_gt_paths,
-    val_image_paths,
-    val_gt_paths,
-):
-    n_rays = 32
+def train_dsb():
+    path = "/scratch/share/cidas/cca/data/dsb"
 
-    use_gpu = False and gputools_available()
+    train_image_paths = natsorted(glob(os.path.join(path, "train", "images", "*.tif")))
+    train_gt_paths = natsorted(glob(os.path.join(path, "train", "masks", "*.tif")))
+    val_image_paths = natsorted(glob(os.path.join(path, "test", "images", "*.tif")))
+    val_gt_paths = natsorted(glob(os.path.join(path, "test", "masks", "*.tif")))
 
-    grid = (2, 2)
-
-    n_channel = 1
-
-    configuration = Config2D(
-        n_rays=n_rays,
-        grid=grid,
-        use_gpu=use_gpu,
-        n_channel_in=n_channel,
-    )
-    print(configuration)
-
-    model = StarDist2D(configuration, name='stardist', basedir='models')
-    model.train(
-        X_trn, Y_trn, validation_data=(X_val,Y_val), augmenter=augmenter
+    run_stardist_training(
+        train_image_paths=train_image_paths,
+        train_label_paths=train_gt_paths,
+        val_image_paths=val_image_paths,
+        val_label_paths=val_gt_paths,
+        use_gpu=True,
     )
 
-    # Threshold optimization
-    model.optimize_thresholds(X_val, Y_val)
+
+train_dsb()
--- tukra/training/__init__.py
+++ tukra/training/__init__.py
@@ -1,1 +1,2 @@
 from .cellpose2 import run_cellpose2_finetuning
+from .stardist import run_stardist_training
