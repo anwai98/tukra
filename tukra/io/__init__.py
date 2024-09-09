115753cd82d573d238e55c012a11ca7a2a4f917c
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-09-09 20:16:25 +0530

    Add general nnUNet convenience functionalities (#2)
    * Add general nnUNet convenience functionalities
    
    * Minor fix to dtypes
    
    * Minor fix to labels dir path

--- tukra/io/__init__.py
+++ tukra/io/__init__.py
@@ -1,1 +1,1 @@
-from .util import read_image
+from .util import read_image, write_image
---
+++ tukra/training/nnunet.py
@@ -0,0 +1,93 @@
+import os
+import warnings
+from typing import Union
+
+import torch
+
+
+def declare_paths(nnunet_path: str):
+    """To let the system known of the path variables where the respective folders exist (important for all components)
+
+    Args:
+        nnunet_path: The path where the expected files will be used by nnUNet for training and inference.
+    """
+    warnings.warn(
+        "Make sure you have created the directories mentioned in this functions (relative to the root directory)"
+    )
+
+    os.environ["nnUNet_raw"] = os.path.join(nnunet_path, "nnUNet_raw")
+    os.environ["nnUNet_preprocessed"] = os.path.join(nnunet_path, "nnUNet_preprocessed")
+    os.environ["nnUNet_results"] = os.path.join(nnunet_path, "nnUNet_results")
+
+
+def preprocess_data(dataset_id: str, planner: str = "nnUNetPlannerResEncL"):
+    """Function to plan and preprocess the inputs for nnUNet and prepare the training structure.
+
+    Args:
+        dataset_id: The dataset id created for the respective nnUNet-style data folder format.
+        planner: The planner to design the experiments for training nnUNet.
+    """
+    # Let's check the preprocessing first
+    cmd = f"nnUNetv2_plan_and_preprocess -d {dataset_id} -pl {planner} --verify_dataset_integrity"
+    os.system(cmd)
+
+
+def train_nnunetv2(
+    root_dir: Union[os.PathLike, str],
+    dataset_name: str,
+    dataset_id: str,
+    dim: str,
+    plans: str = "nnUNetResEncUNetLPlans",
+    fold: int = 0,
+    have_prepared_splits: bool = True,
+):
+    """Function to train nnUNet in the expected structure.
+
+    Args:
+        root_dir: The parent dir where the nnUNet files are stored.
+        dataset_name: The entire name of the dataset in nnUNet-style (say, `Dataset999_XYZ`)
+        dataset_id: The dataset id created for the respective nnUNet-style data folder format.
+        dim: The nnUNet configuration (2d / 3d_fullres) to train.
+        plans: The plan designed for the experiments for training nnUNet.
+        fold: The fold of choice for the 5-fold cross-validation based nnUNet training.
+        have_prepared_splits: Whether the train-val splits and the respective split file for training are prepared.
+    """
+    if have_prepared_splits:
+        # It's expected that you create folds by yourself in your desired train-val structure.
+        _split_file_exists = os.path.exists(
+            os.path.join(root_dir, "nnUNet_preprocessed", dataset_name, "splits_final.json")
+        )
+        assert _split_file_exists, "The experiment expects you to create the splits yourself."
+
+    # Train your own nnUNet
+    gpus = torch.cuda.device_count()
+    cmd = f"nnUNet_compile=T nnUNet_n_proc_DA=8 nnUNetv2_train {dataset_id} {dim} {fold} -num_gpus {gpus} --c "
+    cmd += f"-p {plans}"
+    os.system(cmd)
+
+
+def predict_nnunetv2(
+    root_dir: Union[os.PathLike, str],
+    dataset_name: str,
+    dataset_id: str,
+    dim: str,
+    plans: str = "nnUNetResEncUNetLPlans",
+    fold: int = 0
+):
+    """Function to predict using trained nnUNet in the expected structure.
+
+    Args:
+        root_dir: The parent dir where the nnUNet files are stored.
+        dataset_name: The entire name of the dataset in nnUNet-style (say, `Dataset999_XYZ`)
+        dataset_id: The dataset id created for the respective nnUNet-style data folder format.
+        dim: The nnUNet configuration (2d / 3d_fullres) to train.
+        plans: The plan designed for the experiments for training nnUNet.
+        fold: The fold for training nnUNet.
+    """
+    input_dir = os.path.join(root_dir, "nnUNet_raw", dataset_name, "imagesTs")
+    assert os.path.exists(input_dir), "The input folder does not exists."
+
+    output_dir = os.path.join(root_dir, "nnUNet_raw", dataset_name, "predictionTs")
+
+    cmd = f"nnUNetv2_predict -i {input_dir} -o {output_dir} -d {dataset_id} -c {dim} -f {fold} -p {plans}"
+    os.system(cmd)
