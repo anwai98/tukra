02b483303cbaa9cf95c25d7366560d07075a87fa
Anwai Archit <anwai.archit@gmail.com>
2025-10-08 17:27:42 +0200

    Update setup.py for find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import runpy
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 __version__ = runpy.run_path("tukra/__version__.py")["__version__"]
@@ -13,7 +13,7 @@ setup(
     author='Anwai Archit',
     author_email='anwai.archit@uni-goettingen.de',
     url='https://github.com/anwai98/tukra',
-    packages=['tukra'],
+    packages=find_packages(include=['tukra', 'tukra.*']),
     license="MIT",
     entry_points={
         "console_scripts": [
